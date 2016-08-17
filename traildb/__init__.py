2b48401378735f93edc23bf9488e546eaf5a4c4c
Greg Nisbet <greg.nisbet@adroll.com>
2016-08-17 16:15:08 -0700

    No Makefile Fix __init__.py

--- traildb/__init__.py
+++ traildb/__init__.py
@@ -1,1 +1,1 @@
-from .traildb import TrailDBError, TrailDBConstructor, TrailDB, TrailDBCursor, tdb_item_field, tdb_item_val
+from .traildb import TrailDBError, TrailDBConstructor, TrailDB, TrailDBCursor, TrailDBEventFilter, tdb_item_field, tdb_item_val
