03261b2019460655dbce3d4fb4fe5f8bb42c0b73
kayibal <alan.f.hoeng@gmail.com>
2017-03-12 22:34:41 +0100

    Add future dependency to setup.py

--- setup.py
+++ setup.py
@@ -4,4 +4,5 @@ setup(name='traildb',
       version='0.0.1',
       description='TrailDB stores and queries cookie trails from raw logs.',
       author='AdRoll.com',
+      install_requires=['future>=0.16.0'],
       packages=['traildb'])
